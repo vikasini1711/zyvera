77029774f95a9becd96dd4c7adbfcdea2ec64c22
vikasini1711 <svikasini2005@gmail.com>
2026-06-12 02:54:01 -0700

    fix env and deployment issues

--- frontend/src/components/player/SongList.jsx
+++ frontend/src/components/player/SongList.jsx
@@ -1,55 +1,39 @@
+import React, { useContext } from "react";
+import { PlayerContext } from "../../context/PlayerContext";
 import "../../css/mainArea/SongList.css";
 
 const SongList = ({ songs }) => {
-  if (!songs || songs.length === 0) {
-    return (
-      <div className="songlist-root">
-        <div className="songlist-empty">
-          <p className="songlist-empty-text">🎵 No songs found</p>
-          <span className="songlist-empty-subtext">
-            Try searching with a different keyword
-          </span>
-        </div>
-      </div>
-    );
-  }
+  const { playSong } = useContext(PlayerContext);
+
   return (
     <div className="songlist-root">
       <div className="songlist-scroll">
-        {songs.length === 0 && (
-          <p className="searchbar-empty">Search songs to display 🎧</p>
-        )}
-
         <table className="songlist-table">
-          <colgroup>
-            <col className="col-index" />
-            <col className="col-name" />
-            <col className="col-artist" />
-            <col className="col-year" />
-            <col className="col-duration" />
-          </colgroup>
-
           <thead>
             <tr>
-              <th className="songlist-th th-index">No</th>
-              <th className="songlist-th">Name</th>
-              <th className="songlist-th">Artist</th>
-              <th className="songlist-th">Year</th>
-              <th className="songlist-th th-duration">Duration</th>
+              <th>No</th>
+              <th>Name</th>
+              <th>Artist</th>
+              <th>Action</th>
             </tr>
           </thead>
 
           <tbody>
             {songs.map((song, index) => (
               <tr key={song.id}>
-                <td className="songlist-td td-index">{index + 1}</td>
-                <td className="songlist-td">{song.name}</td>
-                <td className="songlist-td">{song.artist_name}</td>
-                <td className="songlist-td">{song.releasedate}</td>
-                <td className="songlist-td td-duration">{song.duration}</td>
+                <td>{index + 1}</td>
+                <td>{song.name}</td>
+                <td>{song.artist_name}</td>
+
+                <td>
+                  <button onClick={() => playSong(song)}>
+                    ▶ Play
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
+
         </table>
       </div>
     </div>
