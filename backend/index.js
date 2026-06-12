bce69b9595023beab374d7cc84965ffc0464c0aa
vikasini1711 <svikasini2005@gmail.com>
2026-06-12 03:26:22 -0700

    replace backend file

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 //connect your Database
 connectDB();
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "http://localhost:5173",
     credentials:true,
 })
 );
