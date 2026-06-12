73077a772c20fa8a6278a726afd45746b0862982
vikasini1711 <svikasini2005@gmail.com>
2026-06-12 03:06:12 -0700

    rollback player context fix

--- frontend/src/components/player/SongList.jsx
+++ frontend/src/components/player/SongList.jsx
@@ -1,41 +1,20 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { PlayerContext } from "../../context/PlayerContext";
-import "../../css/mainArea/SongList.css";
 
 const SongList = ({ songs }) => {
   const { playSong } = useContext(PlayerContext);
 
   return (
-    <div className="songlist-root">
-      <div className="songlist-scroll">
-        <table className="songlist-table">
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Name</th>
-              <th>Artist</th>
-              <th>Action</th>
-            </tr>
-          </thead>
+    <div>
+      {songs.map((song) => (
+        <div key={song.id}>
+          <span>{song.name}</span>
 
-          <tbody>
-            {songs.map((song, index) => (
-              <tr key={song.id}>
-                <td>{index + 1}</td>
-                <td>{song.name}</td>
-                <td>{song.artist_name}</td>
-
-                <td>
-                  <button onClick={() => playSong(song)}>
-                    ▶ Play
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-
-        </table>
-      </div>
+          <button onClick={() => playSong(song)}>
+            Play
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
