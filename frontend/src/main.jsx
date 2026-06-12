bbc6dd7723f8bcfaa2d37d4bd7ab3f551f37235d
vikasini1711 <svikasini2005@gmail.com>
2026-06-12 03:02:48 -0700

    fix PlayerContext export issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+
 import App from "./App.jsx";
 import store from "./redux/slices/store.js";
 
-createRoot(document.getElementById('root')).render(
+import { PlayerProvider } from "./context/PlayerContext.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-       <App />
-
+      <PlayerProvider>
+        <App />
+      </PlayerProvider>
     </Provider>
-    
-    
-   
-  </StrictMode>,
+  </StrictMode>
 );
