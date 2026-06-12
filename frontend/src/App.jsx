440f4259d43664eed6fec956a6e0de87c4940a6d
vikasini1711 <svikasini2005@gmail.com>
2026-06-12 02:12:24 -0700

    fix Login/Register undefined error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
-//import Login from "./pages/Login";
-//import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 import "./App.css";
 
